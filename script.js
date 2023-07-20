2a74315d1103f27e767d2bb7d40de8761fe88087
aayushchauhan10 <ai.aayush10@gmail.com>
2023-07-20 20:01:52 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -6,6 +6,17 @@
 //   sliderInner.style.transform = `translateX(-${slideIndex * 100}%)`;
 // }, 3000);
 
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () => {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
+
 const sliderInner = document.querySelector(".slider-inner");
 const slides = document.querySelectorAll(".slide");
 const slideCount = slides.length;
@@ -15,7 +26,7 @@ let interval;
 function startSlider() {
   interval = setInterval(() => {
     goToSlide(currentIndex + 1);
-  }, 3000);
+  }, 5000);
 }
 
 function goToSlide(index) {
@@ -28,6 +39,7 @@ startSlider();
 
 const playButton = document.getElementById("playButton");
 const videoContainer = document.getElementById("videoContainer");
+const aboutContent = document.getElementById("about-content");
 const video = document.createElement("video");
 let isVideoLoaded = false;
 
@@ -57,37 +69,56 @@ playButton.addEventListener("click", () => {
     if (video.paused) {
       video.play();
       playButton.classList.add("playing");
+      aboutContent.classList.add("hiddenContent");
     } else {
       video.pause();
       playButton.classList.remove("playing");
     }
   }
 });
+let buttonIcon; // Declare the buttonIcon variable outside the toggleDropdown() function
+
 function toggleDropdown(button) {
   const item = button.closest(".item");
   const dropdownContent = item.querySelector(".dropdown-content");
   dropdownContent.classList.toggle("show");
+
+  // Get the button icon element
+  buttonIcon = button.querySelector(".button-icon");
+
+  // Toggle the icon based on the dropdown visibility
+  if (dropdownContent.classList.contains("show")) {
+    buttonIcon.src = "./images/cancel.png";
+  } else {
+    buttonIcon.src = "./images/arrowsmall.png";
+  }
 }
 
 document.addEventListener("click", (event) => {
   const dropdownContents = document.querySelectorAll(".dropdown-content");
-  const expandButtons = document.querySelectorAll(".expand-button");
 
-  for (let i = 0; i < dropdownContents.length; i++) {
+  dropdownContents.forEach((dropdownContent) => {
+    const item = dropdownContent.closest(".item");
+    const button = item.querySelector(".expand-button");
+    const buttonIcon = button.querySelector(".button-icon"); // Get the button icon element
+
     if (
-      !event.target.closest(".item") &&
-      dropdownContents[i].classList.contains("show")
+      !button.contains(event.target) &&
+      dropdownContent.classList.contains("show")
     ) {
-      dropdownContents[i].classList.remove("show");
+      dropdownContent.classList.remove("show");
+      buttonIcon.src = "./images/arrowsmall.png"; // Set the icon back to the initial state when the dropdown is closed
     }
-  }
+  });
 });
+// With this change, the buttonIcon variable is accessible within both functions, and the button icon should now change appropriately when the dropdown content is shown or hidden.
 
 function initializeTestimonials() {
   const testimonials = document.querySelectorAll(".testimonial");
   testimonials.forEach((testimonial, index) => {
     if (index < 3) {
       testimonial.classList.remove("hidden");
+      testimonial.classList.add("blur");
       if (index === 1) {
         testimonial.classList.remove("blur");
         testimonial.style.transform = "scale(1)";
@@ -134,4 +165,33 @@ function showPrevious() {
   testimonials[0].classList.add("hidden");
 
   initializeTestimonials();
+}
+
+function toggleItinerary(button) {
+  const item = button.closest(".item");
+  const itineraryDetails = item.querySelector(".itinerary-details");
+
+  // Toggle the .flipped class on the item
+  item.classList.toggle("flipped");
+
+  // Toggle the .hide class for the itinerary details
+  itineraryDetails.classList.toggle("hide");
+
+  // Close other open itinerary details
+  closeItinerary(item);
+}
+
+function closeItinerary(exceptItem) {
+  const items = document.querySelectorAll(".item");
+
+  // Close all itinerary details and remove the .flipped class from all items except the clicked one
+  items.forEach((item) => {
+    if (item !== exceptItem) {
+      item.classList.remove("flipped");
+      const itineraryDetails = item.querySelector(".itinerary-details");
+      if (!itineraryDetails.classList.contains("hide")) {
+        itineraryDetails.classList.add("hide");
+      }
+    }
+  });
 }
